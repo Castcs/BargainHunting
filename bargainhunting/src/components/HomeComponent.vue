446b73080d48b16060ce062f7512fd96584d3f61
schulteiss20 <143538995+schulteiss20@users.noreply.github.com>
2023-11-09 11:24:46 -0500

    Update HomeComponent.vue
    made table instead of boxes

--- bargainhunting/src/components/HomeComponent.vue
+++ bargainhunting/src/components/HomeComponent.vue
@@ -1,35 +1,62 @@
 <template>
-    <div class="parent-container">
-        <!-- <div v-for="(component, index) in components" :key="index" class="contentArea"> -->
-            <SearchResult v-for="(component, index) in components" :key="index"
-             @remove="removeComponent(index)"
-             :title="component.title"
-             :price="component.price"/>
-        <!-- </div> -->
-    </div>
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Store</th>
+          <th>Price</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(component, index) in components" :key="index">
+          <td>{{ component.title }}</td>
+          <td>{{ component.store }}</td>
+          <td>${{ component.price }}</td>
+          <td>
+            <button @click="removeComponent(index)">Remove</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import SearchResult from './SearchResult.vue'
-export default {
-    name : 'HomeComponent',
-    components: {
-        SearchResult,
-    },
-    data() {
-        return {
-            components: [ {title: 'MacBook Pro', price: '849.99'},
-                          {title: 'AirPods', price: '237.00'},
-                          {title: 'Pen', price: '4.99'}], // Array to track instances of MyComponent
-        };
-    },
-    methods: {
-        removeComponent(index) {
-        // Remove the component at the specified index
-            this.components.splice(index, 1);
-        },
-    },
+// Assuming SearchResult.vue is not being used for this component anymore.
+// If it is, you can leave the import and components registration in place.
 
-}
+export default {
+  name: 'HomeComponent',
+  data() {
+    return {
+      components: [
+        { title: 'MacBook Pro', store: 'Amazon', price: '849.99' },
+        { title: 'AirPods', store: 'Amazon', price: '237.00' },
+        { title: 'Pen', store: 'Amazon', price: '4.99' },
+      ],
+    };
+  },
+  methods: {
+    removeComponent(index) {
+      this.components.splice(index, 1);
+    },
+  },
+};
 </script>
+
+<style>
+/* Add any additional styles for your table here */
+.table {
+  width: 100%;
+  border-collapse: collapse;
+}
+.table th,
+.table td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+</style>
 
